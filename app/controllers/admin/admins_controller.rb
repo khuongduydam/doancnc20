63545ea5b6934c3c2bf65b67f39b51a81735eb51
ron <ron@vinova.sg>
2016-11-16 22:40:24 +0700

    change admins to users

--- app/controllers/admin/admins_controller.rb
+++
@@ -1,7 +0,0 @@
-class Admin::AdminsController < AdminsController
-  def index
-  end
-
-  def show
-  end
-end
---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,7 @@
+class Admin::UsersController < AdminsController
+  def index
+  end
+
+  def show
+  end
+end
