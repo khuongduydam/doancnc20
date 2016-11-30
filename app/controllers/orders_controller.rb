4751a6f5556a01234d0135ca6dfe7fcaf13d4f71
ron <ron@vinova.sg>
2016-11-30 16:56:03 +0700

    feature cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,11 +3,47 @@ class OrdersController < ApplicationController
   before_action :set_cart, only: [:new, :create]
   before_action :find_order, only: [:show, :edit, :update, :destroy]
 
+  def index
+    
+  end
+
+  def show
+
+  end
+  
   def new
     if @cart.order_items.empty?
       redirect_to root_path
       return
     end
     @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.add_order_items_from_cart(@cart)
+    if @order.save
+      Cart.destroy(session[:cart_id])
+      session[:cart_id] = nil
+      flash[:success] = "Thank you for your order"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :address, :phone, :email, :pay_type)
+  end
+
+  def find_order
+    begin
+      @order = Order.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:error] = "Order not found"
+      redirect_to root_path
+    end  
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,13 @@
 class Order < ApplicationRecord
+  has_many :order_items, dependent: :destroy
+  PAYMENT_TYPES = ['Paypal','Direct']
+  validates :name, :address, :email, :phone, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
+
+  def add_order_items_from_cart(cart)
+    cart.order_items.each do |item|
+      order_items << item
+      item.cart_id = nil
+    end
+  end
 end
