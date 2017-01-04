0578524d067333d5b68ef85f20d2383720cd13fc
computer <ron@vinova.sg>
2017-01-04 10:02:43 -0500

    fix bug

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module FruitShop
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     I18n.enforce_available_locales = true
-    #config.i18n.default_locale = :vi
+    config.i18n.default_locale = :en
   end
 end
 
