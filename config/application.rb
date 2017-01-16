d9d95ee34e571557866e54ad5c8cbbb16f2de4cf
ron <ron@vinova.sg>
2017-01-16 18:23:09 +0700

    fix ckeditor

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module FruitShop
     # -- all .rb files in that directory are automatically loaded.
     I18n.enforce_available_locales = true
     config.i18n.default_locale = :en
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
 
