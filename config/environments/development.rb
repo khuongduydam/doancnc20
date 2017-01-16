f55c8b81ff34a294f0724f63aee2fb273e79581a
ron <ron@vinova.sg>
2017-01-16 16:49:25 +0700

    config host send mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   #For Devise
-  config.action_mailer.default_url_options = { host: 'fruit-app.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'localhost:3000'}
   config.action_mailer.delivery_method = :smtp
 end
